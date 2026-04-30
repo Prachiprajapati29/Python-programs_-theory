35b58518f7bd4056c299d322b113236ed8f5ccff
Prachi prajapati <prachipra.2929@outlook.com>
2026-04-30 03:36:40 +0100

    Add files via upload

---
+++ python33.py
@@ -0,0 +1,4 @@
+minutes=int(input("Enter total minutes: "))
+hours=minutes //60
+remaining= minutes%60
+print(hours,"hours and",remaining,"minutes")
---
+++ python44.py
@@ -0,0 +1,6 @@
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+a,b=b,a
+print("after swapping:")
+print("a=",a)
+print("b=",b)
